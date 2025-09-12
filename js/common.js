d3520a3f0c38c6047be4e046b12ba6b22fac27ab
20240421 <157558098+20240421@users.noreply.github.com>
2025-09-12 14:09:28 +0900

    Add files via upload

---
+++ js/common.js
@@ -0,0 +1,245 @@
+$(function () {
+  $(".slide-show1").slick({
+    autoplay: true, // 自動でスクロール
+    autoplaySpeed: 0, // 自動再生のスライド切り替えまでの時間を設定
+    speed: 10000, // スライドが流れる速度を設定
+    cssEase: "linear", // スライドの流れ方を等速に設定
+    slidesToShow: 4, // 表示するスライドの数
+    swipe: false, // 操作による切り替えはさせない
+    arrows: false, // 矢印非表示
+    pauseOnFocus: false, // スライダーをフォーカスした時にスライドを停止させるか
+    pauseOnHover: false, // スライダーにマウスホバーした時にスライドを停止させるか
+    responsive: [
+      {
+        breakpoint: 750,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 550,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+  });
+});
+
+$(function () {
+  $(".slide-show2").slick({
+    autoplay: true, // 自動でスクロール
+    autoplaySpeed: 0, // 自動再生のスライド切り替えまでの時間を設定
+    speed: 10000, // スライドが流れる速度を設定
+    cssEase: "linear", // スライドの流れ方を等速に設定
+    slidesToShow: 4, // 表示するスライドの数
+    swipe: false, // 操作による切り替えはさせない
+    arrows: false, // 矢印非表示
+    pauseOnFocus: false, // スライダーをフォーカスした時にスライドを停止させるか
+    pauseOnHover: false, // スライダーにマウスホバーした時にスライドを停止させるか
+    rtl: true, // スライダーを左から右に流す（逆向き）
+    responsive: [
+      {
+        breakpoint: 750,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 550,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+  });
+});
+
+$(function () {
+  $(".slide-show1-sp").slick({
+    autoplay: true, // 自動でスクロール
+    autoplaySpeed: 0, // 自動再生のスライド切り替えまでの時間を設定
+    speed: 10000, // スライドが流れる速度を設定
+    cssEase: "linear", // スライドの流れ方を等速に設定
+    slidesToShow: 4, // 表示するスライドの数
+    swipe: false, // 操作による切り替えはさせない
+    arrows: false, // 矢印非表示
+    pauseOnFocus: false, // スライダーをフォーカスした時にスライドを停止させるか
+    pauseOnHover: false, // スライダーにマウスホバーした時にスライドを停止させるか
+    responsive: [
+      {
+        breakpoint: 750,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 550,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+  });
+});
+
+//アコーディオンをクリックした時の動作
+$(".title").on("click", function () {
+  //タイトル要素をクリックしたら
+  $(".box").slideUp(500); //クラス名.boxがついたすべてのアコーディオンを閉じる
+
+  var findElm = $(this).next(".box"); //タイトル直後のアコーディオンを行うエリアを取得
+
+  if ($(this).hasClass("close")) {
+    //タイトル要素にクラス名closeがあれば
+    $(this).removeClass("close"); //クラス名を除去
+  } else {
+    //それ以外は
+    $(".close").removeClass("close"); //クラス名closeを全て除去した後
+    $(this).addClass("close"); //クリックしたタイトルにクラス名closeを付与し
+    $(findElm).slideDown(500); //アコーディオンを開く
+  }
+});
+
+$(function () {
+  // スクロールを開始したら
+  $(window).on("scroll", function () {
+    // ファーストビューの高さを取得
+    mvHeight = $(".js-mv").height();
+    if ($(window).scrollTop() > mvHeight) {
+      // スクロールの位置がファーストビューより下の場合にclassを付与
+      $(".js-header").addClass("transform");
+      $(".hbg-btn").addClass("transform1");
+    } else {
+      // スクロールの位置がファーストビューより上の場合にclassを外す
+      $(".js-header").removeClass("transform");
+      $(".hbg-btn").removeClass("transform1");
+    }
+  });
+});
+
+$(".openbtn").click(function () {
+  //ボタンがクリックされたら
+  $(this).toggleClass("active"); //ボタン自身に activeクラスを付与
+  $("#g-nav").fadeToggle(100);
+  $("#g-nav").toggleClass("panelactive"); //ナビゲーションにpanelactiveクラスを付与
+  $("body").toggleClass("noscroll"); //ナビゲーションにpanelactiveクラスを付与
+});
+
+$("#g-nav a[href]").on("click", function (event) {
+  $(".openbtn").trigger("click");
+});
+
+$("#g-nav a").click(function () {
+  //ナビゲーションのリンクがクリックされたら
+  $(".openbtn").removeClass("active"); //ボタンの activeクラスを除去
+  $("#g-nav").removeClass("panelactive"); //ナビゲーションのpanelactiveクラスも除去
+  $("body").removeClass("noscroll"); //ナビゲーションのpanelactiveクラスも除去
+});
+
+$('#page-link a[href*="#"]').click(function () {
+  //全てのページ内リンクに適用させたい場合はa[href*="#"]のみでもOK
+  var elmHash = $(this).attr("href"); //ページ内リンクのHTMLタグhrefから、リンクされているエリアidの値を取得
+  var pos = $(elmHash).offset().top; //idの上部の距離を取得
+  $("body,html").animate({ scrollTop: pos }, 500); //取得した位置にスクロール。500の数値が大きくなるほどゆっくりスクロール
+  return false;
+});
+
+$(window).on("load", function () {
+  $("#splash-logo").delay(1200).fadeOut("slow"); //ロゴを1.2秒でフェードアウトする記述
+
+  //=====ここからローディングエリア（splashエリア）を1.5秒でフェードアウトした後に動かしたいJSをまとめる
+  $("#splash")
+    .delay(2000)
+    .fadeOut("slow", function () {
+      //ローディングエリア（splashエリア）を1.5秒でフェードアウトする記述
+
+      $("body").addClass("appear"); //フェードアウト後bodyにappearクラス付与
+    });
+  //=====ここまでローディングエリア（splashエリア）を1.5秒でフェードアウトした後に動かしたいJSをまとめる
+
+  $(".splashbg1").on("animationend", function () {
+    $(function () {
+      $(".slider")
+        // 最初のスライドに"add-animation"のclassを付ける(data-slick-index="0"が最初のスライドを指す)
+        .on("init", function () {
+          $('.slick-slide[data-slick-index="0"]').addClass("add-animation");
+        })
+        // 通常のオプション
+        .slick({
+          autoplay: true, // 自動再生ON
+          fade: true, // フェードON
+          arrows: false, // 矢印OFF
+          speed: 2000, // スライド、フェードアニメーションの速度2000ミリ秒
+          autoplaySpeed: 4000, // 自動再生速度4000ミリ秒
+          pauseOnFocus: false, // フォーカスで一時停止OFF
+          pauseOnHover: false, // マウスホバーで一時停止OFF
+        })
+        .on({
+          // スライドが移動する前に発生するイベント
+          beforeChange: function (event, slick, currentSlide, nextSlide) {
+            // 表示されているスライドに"add-animation"のclassをつける
+            $(".slick-slide", this).eq(nextSlide).addClass("add-animation");
+            // あとで"add-animation"のclassを消すための"remove-animation"classを付ける
+            $(".slick-slide", this)
+              .eq(currentSlide)
+              .addClass("remove-animation");
+          },
+          // スライドが移動した後に発生するイベント
+          afterChange: function () {
+            // 表示していないスライドはアニメーションのclassを外す
+            $(".remove-animation", this).removeClass(
+              "remove-animation add-animation"
+            );
+          },
+        });
+    });
+  });
+});
+
+$(function () {
+  // スクロールイベント
+  $(window).scroll(function () {
+    // フェードアニメーションを呼び出す
+    fadeAnime();
+  });
+
+  // フェードアニメーションの設定
+  function fadeAnime() {
+    // .fadeUpTriggerが付いた要素に対して
+    $(".blurTrigger").each(function () {
+      // 縦位置を取得し-50pxして、変数targetに代入する
+      let target = ($(this).offset().top -= 50);
+      // スクロール量を取得し、変数scrollに代入する
+      let scroll = $(window).scrollTop();
+      // 表示画面の高さを取得し、変数windowHeightに代入する
+      let windowHeight = $(window).height();
+
+      // 要素の縦位置から表示画面の高さ+200pxを引いた数字よりscrollのほうが大きい場合
+      if (scroll > target - windowHeight + 200) {
+        // .blurを追加する
+        $(this).addClass("blur");
+      }
+    });
+  }
+});
+
+$(function () {
+  $("body").fadeIn(2000); //ページを2秒かけて表示する
+});
+
+// 画像のモーダル
+$(".gallery").modaal({
+  type: "image",
+  overlay_close: true, //モーダル背景クリック時に閉じるか
+  before_open: function () {
+    // モーダルが開く前に行う動作
+    $("html").css("overflow-y", "hidden"); /*縦スクロールバーを出さない*/
+    $("header").css("z-index", "auto");
+  },
+  after_close: function () {
+    // モーダルが閉じた後に行う動作
+    $("html").css("overflow-y", "scroll"); /*縦スクロールバーを出す*/
+    $("header").css("z-index", "9999");
+  },
+});
